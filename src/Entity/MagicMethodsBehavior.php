db486f4997f92237c14b39accf324c9093c20892
vidmed <mr.vidmed@gmail.com>
2015-12-09 13:00:24 +0200

    правка магических методов

--- src/Entity/MagicMethodsBehavior.php
+++ src/Entity/MagicMethodsBehavior.php
@@ -11,10 +11,11 @@ trait MagicMethodsBehavior
         $extraSetter = 'set' . join('', array_map(function ($piece) {
                 return ucfirst($piece);
             }, explode("_", $name)));
-        if (method_exists($this, $setter))
+        if (method_exists($this, $setter)) {
             return $this->$setter($value);
-        elseif (method_exists($this, $extraSetter))
+        } elseif (method_exists($this, $extraSetter)) {
             return $this->$extraSetter($value);
+        }
 
         throw new \Exception('Property "' . get_class($this) . '.' . $name . '" is not defined.');
     }
@@ -24,14 +25,32 @@ trait MagicMethodsBehavior
         $getter = 'get' . $name;
 
         $extraGetter = 'get' . join('', array_map(function ($piece) {
-            return ucfirst($piece);
-        }, explode("_", $name)));
+                return ucfirst($piece);
+            }, explode("_", $name)));
 
-        if (method_exists($this, $getter))
+        if (method_exists($this, $getter)) {
             return $this->$getter();
-        elseif (method_exists($this, $extraGetter))
+        } elseif (method_exists($this, $extraGetter)) {
             return $this->$extraGetter();
+        }
 
         throw new \Exception('Property "' . get_class($this) . '.' . $name . '" is not defined.');
+    }
+
+    public function __isset($name)
+    {
+        $getter = 'get' . $name;
+
+        $extraGetter = 'get' . join('', array_map(function ($piece) {
+                return ucfirst($piece);
+            }, explode("_", $name)));
+
+        if (method_exists($this, $getter)) {
+            return $this->$getter() !== null;
+        } elseif (method_exists($this, $extraGetter)) {
+            return $this->$extraGetter() !== null;
+        }
+
+        return false;
     }
 }
