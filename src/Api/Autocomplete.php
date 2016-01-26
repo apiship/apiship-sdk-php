5dc5f500624206fc56011a8a0439e527b3414dbc
Ilya Khalturinskiy <khalturinsky@gmail.com>
2016-01-26 13:27:07 +0300

    Для autocomplete добавлен параметр int

--- src/Api/Autocomplete.php
+++ src/Api/Autocomplete.php
@@ -17,12 +17,17 @@ class Autocomplete extends AbstractApi
      * Автодополнение по наименованию региона
      *
      * @param string $query Наименование региона
+     * @param int    $limit
      *
      * @return GetRegionResponse
      */
-    public function getRegion($query)
+    public function getRegion($query, $limit = 20)
     {
-        $resultJson = $this->adapter->get('autocomplete/region/' . urlencode(trim($query)));
+        if (!is_int($limit) || $limit < 0) {
+            $limit = 20;
+        }
+
+        $resultJson = $this->adapter->get('autocomplete/region/' . urlencode(trim($query)), [], ['limit' => $limit]);
         $result     = json_decode($resultJson);
 
         $response = new GetRegionResponse();
@@ -51,12 +56,17 @@ class Autocomplete extends AbstractApi
      * Автодополнение района
      *
      * @param string $query Наименование района
+     * @param int    $limit
      *
      * @return GetAreaResponse
      */
-    public function getArea($query)
+    public function getArea($query, $limit = 20)
     {
-        $resultJson = $this->adapter->get('autocomplete/area/' . urlencode(trim($query)));
+        if (!is_int($limit) || $limit < 0) {
+            $limit = 20;
+        }
+
+        $resultJson = $this->adapter->get('autocomplete/area/' . urlencode(trim($query)), [], ['limit' => $limit]);
         $result     = json_decode($resultJson);
 
         $response = new GetAreaResponse();
@@ -85,12 +95,17 @@ class Autocomplete extends AbstractApi
      * Автодополнение по городу
      *
      * @param string $query Наименование населённого пункта
+     * @param int    $limit
      *
      * @return GetCityResponse
      */
-    public function getCity($query)
+    public function getCity($query, $limit = 20)
     {
-        $resultJson = $this->adapter->get('autocomplete/city/' . urlencode(trim($query)));
+        if (!is_int($limit) || $limit < 0) {
+            $limit = 20;
+        }
+
+        $resultJson = $this->adapter->get('autocomplete/city/' . urlencode(trim($query)), [], ['limit' => $limit]);
         $result     = json_decode($resultJson);
 
         $response = new GetCityResponse();
@@ -121,12 +136,18 @@ class Autocomplete extends AbstractApi
      *
      * @param string $query Наименование населённого пункта
      * @param string $guid  GUID города в системе ФИАС
+     * @param int    $limit
      *
      * @return GetStreetResponse
      */
-    public function getStreet($query, $guid)
+    public function getStreet($query, $guid, $limit = 20)
     {
-        $resultJson = $this->adapter->get('autocomplete/street/' . urlencode(trim($query)), [], ['guid' => $guid]);
+        if (!is_int($limit) || $limit < 0) {
+            $limit = 20;
+        }
+
+        $resultJson = $this->adapter->get('autocomplete/street/' . urlencode(trim($query)), [],
+            ['guid' => $guid, 'limit' => $limit]);
         $result     = json_decode($resultJson);
 
         $response = new GetStreetResponse();
