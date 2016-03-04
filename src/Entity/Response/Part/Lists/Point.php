35fcdd298aa492af60d51c2f5fad809f8060c93e
Ilya Khalturinskiy <khalturinsky@gmail.com>
2016-03-04 14:55:03 +0300

    add field name in Point entity

--- src/Entity/Response/Part/Lists/Point.php
+++ src/Entity/Response/Part/Lists/Point.php
@@ -97,6 +97,29 @@ class Point extends AbstractResponsePart
     protected $timetable;
 
     /**
+     * @var string Наименование ПВЗ
+     */
+    protected $name;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
      * @return int
      */
     public function getId()
