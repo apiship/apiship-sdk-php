ba624006d46b315479be4ec35af5826664c0fc93
Nikita Krasnikov <nikitaksv97@gmail.com>
2020-08-01 22:40:20 +0300

    CalculatorRequest add extraParams property

--- src/Entity/Request/CalculatorRequest.php
+++ src/Entity/Request/CalculatorRequest.php
@@ -74,6 +74,13 @@ class CalculatorRequest extends AbstractRequest
      * которые не успели в указанное время, выдаваться не будут. Если не указывать, будет ожидаться ответ от всех.
      */
     public $timeout;
+
+    /**
+     * Массив с доп.параметрами. Синтаксис '{providerKey}.{paramName} => {value}'
+     * Пример. "dpd.providerConnectId": 1234 - использовать для DPD определенное подключение.
+     * @var array
+     */
+    public $extraParams;
     
     /**
      * @return From
@@ -419,4 +426,35 @@ class CalculatorRequest extends AbstractRequest
         $this->timeout = $timeout;
         return $this;
     }
+
+    /**
+     * @return array
+     */
+    public function getExtraParams()
+    {
+        return $this->extraParams;
+    }
+
+    /**
+     * @param array $extraParams
+     *
+     * @return self
+     */
+    public function setExtraParams($extraParams)
+    {
+        $this->extraParams = $extraParams;
+        return $this;
+    }
+    
+    /**
+     * @param array $extraParams
+     *
+     * @return self
+     */
+    public function addExtraParam($extraParam)
+    {
+        $this->extraParams[] = $extraParam;
+        return $this;
+    }
+
 }
